d566d5a81fbdd2785db6984afec19ef9523f06df
maigje98 <65775312+maigje98@users.noreply.github.com>
2020-06-29 15:51:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/.streamlit/
 
-echo "[server]
-headless = true
-const host = '0.0.0.0'
-const port = process.env.PORT || 3000
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
