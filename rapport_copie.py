c113f951deaa6ca122bfeeb26e2b3e489566af2a
maigje98 <65775312+maigje98@users.noreply.github.com>
2020-10-29 15:29:19 +0100

    Add files via upload

---
+++ rapport_copie.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+## Pandas packages
+import streamlit as st
+
+## Visu
+def main():
+    st.title("ESSAI")
+    
+if __name__=="__main__":
+    main()
